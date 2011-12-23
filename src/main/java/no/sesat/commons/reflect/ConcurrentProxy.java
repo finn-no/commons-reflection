8a63e1e473b45f2f521b45dd837cbef3de4ffa61
mck <mick@semb.wever.org>
2011-12-23 08:56:17 +0100

    Return to "GPLv3 with Classpath Exception" so that it remains compatible with OpenJDK (since it is a derivative of th Proxy class). This work is submitted to the Java Bug Database  http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=7123493 so you may also use this work under the Oracle Binary Code License (BCL).

--- src/main/java/no/sesat/commons/reflect/ConcurrentProxy.java
+++ src/main/java/no/sesat/commons/reflect/ConcurrentProxy.java
@@ -1,18 +1,17 @@
 /* Copyright (2007-2011) Schibsted ASA
- *   This file is part of Sesat Commons.
  *
- *   Sesat Commons is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU Lesser General Public License as published by
+ *   This is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ *   (at your option) any later version, with the Classpath Exception.
  *
- *   Sesat Commons is distributed in the hope that it will be useful,
+ *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Lesser General Public License for more details.
+ *   GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU Lesser General Public License
- *   along with Sesat Commons.  If not, see <http://www.gnu.org/licenses/>.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package no.sesat.commons.reflect;
 
